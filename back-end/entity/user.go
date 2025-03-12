779b7e5aa6292ff7fcf69fb93589f747cf23b5ab
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2025-03-12 15:21:11 -0300

    ✨ feat [BACK-END] : Adicionando a struct dos users

---
+++ back-end/entity/user.go
@@ -0,0 +1,19 @@
+package entity
+
+import "time"
+
+type Role string
+
+const (
+	Student Role = "student"
+	Teacher Role = "teacher"
+)
+
+type User struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	Role      Role      `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+}
