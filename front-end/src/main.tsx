db0e6d1df94bc0c4dbb166f386a86068395fcc61
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2025-03-19 17:54:07 -0300

    ✨ feat [FRONT-END]: Adicionando a estrutura das rotas

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import LandingPage from './pages/landingpage.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// Configuração das rotas
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />, // Aqui mostra a LandingPage na raiz "/"
+  },
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <LandingPage />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
