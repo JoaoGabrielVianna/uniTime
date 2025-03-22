3a9083b74f4ff0bee2fa88d8f550a8d8295a1ac4
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2025-03-22 01:44:15 -0300

    ✨ feat [BACK-END]: Organizando o main

--- back-end/cmd/main.go
+++ back-end/cmd/main.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"github.com/joaogabrielvianna/config"
-	"github.com/joaogabrielvianna/controller/users"
 	"github.com/joaogabrielvianna/db"
+
 	"github.com/joaogabrielvianna/router"
 )
 
-var (
-	logger *config.Logger
-)
-
 func main() {
-	logger = config.GetLogger("Main")
-	users.InitializeController()
-	logger.InfoLog("UniTime API rodando...")
+	// Inicializar o logger
+	logger := config.GetLogger("Main")
 
-	// Conectare ao banco de dados
+	// Conectar ao banco de dados
 	db.ConnectDataBase()
 
-	r := router.SetupRouter()
-	r.Run("127.0.0.1:8000")
+	// Iniciar o servidor
+	router.SetupRouter()
+
+	// Iniciar a aplicação
+	logger.InfoLog("UniTime API rodando...")
+
 }
