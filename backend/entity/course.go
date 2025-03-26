dc6a0c604d78342e180e4ae28492c282fba350af
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2025-03-26 16:49:13 -0300

    ✅  update [BACK-END]:  Adicionando alguns parametros para o model course

--- backend/entity/course.go
+++ backend/entity/course.go
@@ -1,12 +1,14 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Course struct {
-	ID          string    `json:"id" gorm:"primaryKey"`
+	ID          uuid.UUID `json:"id" gorm:"primaryKey"`
 	Name        string    `json:"name" gorm:"type:varchar(100)"`
 	Description string    `json:"description" gorm:"type:text"`
-	Years       []Year    `json:"years" gorm:"foreignKey:CourseID"`
-	Users       []User    `json:"users" gorm:"foreignKey:CourseID"`
-	CreatedAt   time.Time `json:"created-at" gorm:"autoCreateTime"`
+	CreatedAt   time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
