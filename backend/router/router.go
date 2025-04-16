c0a8fea969386ead7f53554772f89d527955358c
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2025-04-15 23:30:50 -0300

    ✨ feat [BACK-END]:  Configurando as permissoes de rotas

--- backend/router/router.go
+++ backend/router/router.go
@@ -1,12 +1,17 @@
 package router
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joaogabrielvianna/controller"
 )
 
 func SetupRouter() {
+	// Definir o Gin para rodar no modo 'release' (silencioso)
+	gin.SetMode(gin.ReleaseMode)
+
 	r := gin.Default()
 
 	// Habilita CORS para qualquer origem (ajuste conforme necessário)
@@ -15,8 +20,10 @@ func SetupRouter() {
 			"http://localhost:4000", // Front-end
 			"http://localhost:8000", // API
 		},
-		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders: []string{"Authorization", "Content-Type", "User-Agent"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Authorization", "Content-Type", "User-Agent"},
+		AllowCredentials: true,           // Permitir enviar cookies com requisições
+		MaxAge:           12 * time.Hour, // O tempo que o navegador deve armazenar as permissões CORS
 	}))
 	// Inicializar o controlador (para configurar os logs específicos do controller)
 	controller.InitializeController()
@@ -25,5 +32,5 @@ func SetupRouter() {
 	UserRoutes(r)
 	CourseRoutes(r)
 
-	r.Run("127.0.0.1:8000")
+	r.Run("0.0.0.0:8000")
 }
